988a8106aa4859a17b752ee774180d127229cff0
Marcel Wiessler <marcel@gaisterhand.de>
2021-09-17 22:18:26 +0200

    start custom curves/keyframe drawing

---
+++ package/Editor/Editors/CodeControlCustomCurvesEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor.Timeline;
+using UnityEngine;
+
+namespace Needle.Timeline
+{
+	[CustomTimelineEditor(typeof(CodeControlTrack))]
+	public class CodeControlCustomCurvesEditor : CustomCurvesEditor
+	{
+		public override void OnDrawHeader(Rect rect)
+		{
+			GUI.Label(rect, "Custom header");
+		}
+
+		public override void OnDrawTrack(Rect rect)
+		{
+			GUI.Label(rect, "Custom track " + ActiveRange);
+			if (ActiveRange != null)
+			{
+				var clipRange= ActiveRange;
+				var clipRect = new Rect();
+				clipRect.xMin = clipRange.x;
+				clipRect.xMax = clipRange.y;
+				clipRect.height = 5;
+				clipRect.y = rect.y;
+
+				GUI.DrawTexture(clipRect, Texture2D.redTexture, ScaleMode.StretchToFill, false);
+			}
+		}
+	}
+}
--- projects/Timeline-2020_3/Assets/Empty.cs
+++ projects/Timeline-2020_3/Assets/Empty.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Empty : MonoBehaviour
+public class Empty
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private readonly string test;
+	
 }
+
