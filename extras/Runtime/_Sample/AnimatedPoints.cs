5419890f60d59d23f251b7157ed967a30335f1e9
Marcel Wiessler <marcel@gaisterhand.de>
2021-10-01 11:59:46 +0200

    remove guide tool for now

--- extras/Runtime/_Sample/AnimatedPoints.cs
+++ extras/Runtime/_Sample/AnimatedPoints.cs
@@ -13,45 +13,45 @@ namespace _Sample._Sample
 		public Vector3 End;
 	}
 
-#if UNITY_EDITOR
-	public class GuideTool : CustomClipToolBase
-	{
-		private Vector3? lastPt = null;
-		
-		public override bool Supports(Type type)
-		{
-			return typeof(List<Guide>).IsAssignableFrom(type);
-		}
-
-		protected override void OnToolInput()
-		{
-			if (Event.current.type == EventType.MouseDown && Event.current.modifiers == EventModifiers.None)
-			{
-				if (lastPt != null)
-				{
-					var clip = ActiveClip;
-					var kf = clip.GetClosest((float)CurrentTime);
-					var guide = new Guide() { Start = lastPt.GetValueOrDefault(), End = GetCurrentMousePositionInScene() };
-					if (kf != null && Mathf.Abs((float)CurrentTime - kf.time) < .1f)
-					{
-						var guides = kf.value as List<Guide>;
-						guides.Add(guide);
-						kf.RaiseValueChangedEvent();
-					}
-					else
-					{
-						clip.Add(new CustomKeyframe<List<Guide>>(new List<Guide>() { guide }, (float)CurrentTime));
-					}
-					lastPt = null;
-				}
-				else
-				{
-					lastPt = GetCurrentMousePositionInScene();
-				}
-			}
-		}
-	}
-#endif
+// #if UNITY_EDITOR
+// 	public class GuideTool : CustomClipToolBase
+// 	{
+// 		private Vector3? lastPt = null;
+// 		
+// 		public override bool Supports(Type type)
+// 		{
+// 			return typeof(List<Guide>).IsAssignableFrom(type);
+// 		}
+//
+// 		protected override void OnToolInput()
+// 		{
+// 			if (Event.current.type == EventType.MouseDown && Event.current.modifiers == EventModifiers.None)
+// 			{
+// 				if (lastPt != null)
+// 				{
+// 					var clip = ActiveClip;
+// 					var kf = clip.GetClosest((float)CurrentTime);
+// 					var guide = new Guide() { Start = lastPt.GetValueOrDefault(), End = GetCurrentMousePositionInScene() };
+// 					if (kf != null && Mathf.Abs((float)CurrentTime - kf.time) < .1f)
+// 					{
+// 						var guides = kf.value as List<Guide>;
+// 						guides.Add(guide);
+// 						kf.RaiseValueChangedEvent();
+// 					}
+// 					else
+// 					{
+// 						clip.Add(new CustomKeyframe<List<Guide>>(new List<Guide>() { guide }, (float)CurrentTime));
+// 					}
+// 					lastPt = null;
+// 				}
+// 				else
+// 				{
+// 					lastPt = GetCurrentMousePositionInScene();
+// 				}
+// 			}
+// 		}
+// 	}
+// #endif
 
 	[ExecuteAlways]
 	public class AnimatedPoints : MonoBehaviour, IAnimated
