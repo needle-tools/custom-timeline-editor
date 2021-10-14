b28e013941d9ddfd535bda97d127208a5ae214da
herbst <herbst@prefrontalcortex.de>
2021-10-14 03:16:32 +0200

    tryhard editor modes

---
+++ projects/Timeline-2020_3/Assets/Folder1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Folder1 : MonoBehaviour
+{
+    [ContextMenu(nameof(ToggleModeNow))]
+    private void ToggleModeNow()
+    {
+        ToggleMode();
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log(EditorApplication.applicationContentsPath);
+        Debug.Log(string.Join(", ", ModeService.modeNames));
+        // ToggleMode();
+    }
+
+    [ContextMenu(nameof(SetMyMode))]
+    private void SetMyMode() => SetMode("mymode");
+
+    private static void SetMode(string name)
+    {
+        ModeService.ChangeModeById(name);
+    }
+
+    [MenuItem("Mode/Next")]
+    private static void ToggleMode()
+    {
+        var cur = ModeService.currentIndex;
+        var next = (cur + 1) % (ModeService.modeCount-1);
+        Debug.Log(next);
+        var id = ModeService.modeNames[next];
+        Debug.Log("Enable mode " + id + ", " + next); 
+        ModeService.ChangeModeById(id);
+    }
+}
