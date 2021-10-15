7d3127543687b8e01f21358b7d44ce4e1b24e9c2
Marcel Wiessler <marcel@gaisterhand.de>
2021-10-15 18:22:51 +0200

    quick test adding animation mode

---
+++ package/Editor/Modes/Modes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Needle.Timeline
+{
+    public static class Modes
+    {
+        [InitializeOnLoadMethod]
+        private static void Init()  
+        {
+            DisableAnimationMode();   
+        }
+        
+        [MenuItem("Needle/Timeline/Enable Animation Mode")]
+        private static void EnableAnimationMode()
+        {
+            ModeService.ChangeModeById("animation"); 
+        }
+        
+        [MenuItem("Needle/Timeline/Disable Animation Mode")]
+        private static void DisableAnimationMode()
+        {
+            ModeService.ChangeModeById("default");
+        }
+    }
+}
