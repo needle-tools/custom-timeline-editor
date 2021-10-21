b40babdbca519ecf2bf48fae0f3f17bf2179f188
Marcel Wiessler <marcel@gaisterhand.de>
2021-10-21 11:40:42 +0200

    fix 2021 project

--- package/Runtime/CustomClipTools/ToolModule.cs
+++ package/Runtime/CustomClipTools/ToolModule.cs
@@ -5,8 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using NUnit.Framework;
-using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
