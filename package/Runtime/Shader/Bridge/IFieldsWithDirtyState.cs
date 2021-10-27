18672d28d2f9bbf0feec6fdad3b102a8c5f41fd0
Marcel Wiessler <marcel@gaisterhand.de>
2021-10-27 11:49:48 +0200

    Interface summary

--- package/Runtime/Shader/Bridge/IFieldsWithDirtyState.cs
+++ package/Runtime/Shader/Bridge/IFieldsWithDirtyState.cs
@@ -1,5 +1,8 @@
 ﻿namespace Needle.Timeline
 {
+	/// <summary>
+	/// implement to control when a shader field (list) is updated (e.g. when you have a list of items that should be set once and then manipulated by the shader instead of being set every frame)
+	/// </summary>
 	public interface IFieldsWithDirtyState
 	{
 		bool IsDirty(string fieldName);
