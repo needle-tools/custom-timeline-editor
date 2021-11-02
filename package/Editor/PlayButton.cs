13a21dfc4851bc39dfc902427637d520d8d1ad74
Felix Herbst <herbst@prefrontalcortex.de>
2021-11-02 03:25:36 +0100

    ptr without giant submodules; move data into new playground submodule

--- package/Editor/PlayButton.cs
+++ package/Editor/PlayButton.cs
@@ -1,39 +1,39 @@
-﻿using Needle.Toolbar;
-using UnityEditor;
-using UnityEditor.Timeline;
-using UnityEditorInternal;
-using UnityEngine;
-using UnityEngine.Playables;
-
-namespace Needle.Timeline
-{
-	internal static class CustomTimelinePlayButton
-	{
-		[InitializeOnLoadMethod]
-		private static void Init()
-		{
-			ToolbarButton button = default;
-			button = new ToolbarButton("Play", () =>
-			{
-				var dir = TimelineEditor.masterDirector ? TimelineEditor.masterDirector : Object.FindObjectOfType<PlayableDirector>();
-				if (dir)
-				{
-					switch (dir.state)
-					{
-						case PlayState.Paused:
-							EditorUtility.SetDirty(dir);
-							dir.Play();
-							button.Label.text = "Pause";
-							break;
-						default:
-							EditorUtility.SetDirty(dir);
-							dir.Pause();
-							button.Label.text = "Play";
-							break;
-					}
-				}
-			});
-			button.Add();
-		}
-	}
-}
+﻿// using Needle.Toolbar;
+// using UnityEditor;
+// using UnityEditor.Timeline;
+// using UnityEditorInternal;
+// using UnityEngine;
+// using UnityEngine.Playables;
+//
+// namespace Needle.Timeline
+// {
+// 	internal static class CustomTimelinePlayButton
+// 	{
+// 		[InitializeOnLoadMethod]
+// 		private static void Init()
+// 		{
+// 			ToolbarButton button = default;
+// 			button = new ToolbarButton("Play", () =>
+// 			{
+// 				var dir = TimelineEditor.masterDirector ? TimelineEditor.masterDirector : Object.FindObjectOfType<PlayableDirector>();
+// 				if (dir)
+// 				{
+// 					switch (dir.state)
+// 					{
+// 						case PlayState.Paused:
+// 							EditorUtility.SetDirty(dir);
+// 							dir.Play();
+// 							button.Label.text = "Pause";
+// 							break;
+// 						default:
+// 							EditorUtility.SetDirty(dir);
+// 							dir.Pause();
+// 							button.Label.text = "Play";
+// 							break;
+// 					}
+// 				}
+// 			});
+// 			button.Add();
+// 		}
+// 	}
+// }
