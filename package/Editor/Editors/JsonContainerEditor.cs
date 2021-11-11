d193a4d7d3f3c7c6b2596f944fe3419944db666a
Marcel Wiessler <marcel@gaisterhand.de>
2021-11-11 10:29:09 +0100

    Json container editor

---
+++ package/Editor/Editors/JsonContainerEditor.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Needle.Timeline
+{
+	[CustomEditor(typeof(JsonContainer))]
+	public class JsonContainerEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+			var t = target as JsonContainer;
+			if (!t) return;
+			EditorGUILayout.Space(5);
+			EditorGUILayout.LabelField("Pretty", EditorStyles.boldLabel);
+			var jsonFormatted = JToken.Parse(t.Content).ToString(Formatting.Indented);
+			EditorGUILayout.TextArea(jsonFormatted, GUILayout.ExpandHeight(true));
+		}
+	}
+}
