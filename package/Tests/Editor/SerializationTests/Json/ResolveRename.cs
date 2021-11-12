4d3289eb546f066000ebe56849acb740a7de4b0d
Marcel Wiessler <marcel@gaisterhand.de>
2021-11-12 16:44:38 +0100

    Test that fails for now but should work: refactoring/recovering type

---
+++ package/Tests/Editor/SerializationTests/Json/ResolveRename.cs
@@ -0,0 +1,26 @@
+﻿using Needle.Timeline.Serialization;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Needle.Timeline.Tests.SerializationTests.Json
+{
+	public class RecoveryTests
+	{
+		public class MyNewTypeName
+		{
+			
+		}
+		
+		[Test]
+		public void SerializedType_Rename_IsFound()
+		{
+			const string inputJson = "{\"$type\":\"Needle.Timeline.Tests.SerializationTests.Json.RecoveryTests+MyOldTypeName, Needle.Timeline-Tests\"}";
+			
+			var ser = new NewtonsoftSerializer() { Indented = false };
+			
+			var res = ser.Deserialize<MyNewTypeName>(inputJson);
+
+			Assert.NotNull(res, "TODO: make this work");
+		}
+	}
+}
