b5354d5c9623be9f2df1fc8725dfdab87d9b59f1
Marcel Wiessler <marcel@gaisterhand.de>
2021-11-12 12:45:12 +0100

    added some easing functions

---
+++ package/Runtime/CurveEasing/CircularEasingInOut.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Needle.Timeline.CurveEasing
+{
+	public class CircularInOutEasing : ICurveEasing
+	{
+		// https://github.com/acron0/Easings/blob/master/Easings.css
+		public float Modify(float value)
+		{
+			if(value < 0.5f)
+			{
+				return 0.5f * (1 - Mathf.Sqrt(1 - 4 * (value * value)));
+			}
+			return 0.5f * (Mathf.Sqrt(-((2 * value) - 3) * ((2 * value) - 1)) + 1);
+		}
+	}
+}
---
+++ package/Runtime/CurveEasing/ElasticEasing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Needle.Timeline.CurveEasing
+{
+	// https://github.com/acron0/Easings/blob/master/Easings.cs
+	public class ElasticEasing : ICurveEasing
+	{
+		private const float HALFPI = Mathf.PI / 2f;
+		
+		public float Modify(float value)
+		{
+			if(value < 0.5f)
+			{
+				return 0.5f * Mathf.Sin(13 * HALFPI* (2 * value)) * Mathf.Pow(2, 10 * ((2 * value) - 1));
+			}
+			return 0.5f * (Mathf.Sin(-13 * HALFPI * ((2 * value - 1) + 1)) * Mathf.Pow(2, -10 * (2 * value - 1)) + 2);
+		}
+	}
+}
