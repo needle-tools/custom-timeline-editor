55bce72415ea4e12b95e3b4b355ce49166d3b50a
Marcel Wiessler <marcel@gaisterhand.de>
2021-11-15 17:20:17 +0100

    spread and clump

--- package/Runtime/CustomClipTools/ToolModule/Implementations.cs
+++ package/Runtime/CustomClipTools/ToolModule/Implementations.cs
@@ -98,6 +98,14 @@ namespace Needle.Timeline
 		[Range(0,1)]
 		public float Falloff;
 
+		public SpreadMode Mode;
+
+		public enum SpreadMode
+		{
+			Center = 0,
+			Neighbor = 1,
+		}
+
 		private struct SpreadData
 		{
 			public float RadiusDist;
@@ -120,7 +128,66 @@ namespace Needle.Timeline
 			return ToolInputResult.Failed;
 		}
 
+
 		protected override ToolInputResult OnModifyCaptured(InputData input, List<CapturedModifyContext> captured)
+		{
+			var factor = 0.01f;
+			factor *= Strength;
+			if (input.Button == MouseButton.RightMouse) factor *= -1;
+			
+			switch (Mode)
+			{
+				case SpreadMode.Center:
+					OnSpreadFromCenter(input, captured, factor);
+					break;
+				case SpreadMode.Neighbor:
+					OnSpreadFromNeighbors(input, captured, factor);
+					break;
+			}
+			return ToolInputResult.Success;
+		}
+
+		private static List<Vector3> closestNeighbor = new List<Vector3>();
+		private void OnSpreadFromNeighbors(InputData input, List<CapturedModifyContext> captured, float factor)
+		{
+			closestNeighbor.Clear();
+			for (var index = 0; index < captured.Count; index++)
+			{
+				var e = captured[index];
+				var pos = (Vector3)e.Value.Cast(typeof(Vector3));
+				var closest = default(Vector3);
+				var closestDistance = float.MaxValue;
+				for (var k = 0; k < captured.Count; k++)
+				{
+					if (k == index) continue;
+					var other = captured[k];
+					var otherPos = (Vector3)other.Value.Cast(typeof(Vector3));
+					var dist = Vector3.Distance(pos, otherPos);
+					if (dist < closestDistance)
+					{
+						closestDistance = dist;
+						closest = otherPos;
+					}
+				}
+				closestNeighbor.Add(closest);
+			}
+
+			for (var index = 0; index < captured.Count; index++)
+			{
+				var e = captured[index];
+				var pos = (Vector3)e.Value.Cast(typeof(Vector3));
+				var closest = closestNeighbor[index];
+				var dir = closest - pos;
+				var radiusDist01 = 1f;
+				if (e.Context.AdditionalData is SpreadData data) radiusDist01 = data.RadiusDist;
+				ModifyVector(ref dir, factor, radiusDist01);
+				e.Value = pos - dir;
+				captured[index] = e;
+			}
+			closestNeighbor.Clear();
+		}
+
+		private void OnSpreadFromCenter(InputData input, List<CapturedModifyContext> captured, float factor)
 		{
 			var sum = new Vector3();
 			for (var index = 0; index < captured.Count; index++)
@@ -130,29 +197,28 @@ namespace Needle.Timeline
 				sum += pos;
 			}
 			var center = sum / captured.Count;
-			var factor = 0.01f;
-			factor *= Strength;
-			if (input.Button == MouseButton.RightMouse)
-			{
-				factor *= -1;
-			}
 			for (var index = 0; index < captured.Count; index++)
 			{
 				var e = captured[index];
 				var pos = (Vector3)e.Value.Cast(typeof(Vector3));
 				var dir = center - pos;
-				if (Falloff > 0)
-				{
-					if (e.Context.AdditionalData is SpreadData data)
-						dir *= Mathf.Clamp01((1 - data.RadiusDist)/Falloff);
-				}
-				dir *= factor;
-				dir = Vector3.ClampMagnitude(dir, .1f);
+				var radiusDist01 = 1f;
+				if (e.Context.AdditionalData is SpreadData data) radiusDist01 = data.RadiusDist;
+				ModifyVector(ref dir, factor, radiusDist01);
 				e.Value = pos - dir;
 				captured[index] = e;
 			}
+		}
 
-			return ToolInputResult.Success;
+		private void ModifyVector(ref Vector3 dir, float factor, float radiusDist01)
+		{
+			if (dir.ApproximatelyZeroLength()) dir = Random.insideUnitSphere;
+			if (Falloff > 0)
+			{
+				dir *= Mathf.Clamp01((1 - radiusDist01)/Falloff);
+			}
+			dir *= factor;
+			dir = Vector3.ClampMagnitude(dir, .1f);
 		}
 	}
 
--- package/Runtime/Utils/MathUtils.cs
+++ package/Runtime/Utils/MathUtils.cs
@@ -1,10 +1,22 @@
-﻿namespace Needle.Timeline
+﻿using UnityEngine;
+
+namespace Needle.Timeline
 {
 	public static class MathUtils
 	{
 		public static float Remap(this float value, float from1, float to1, float from2, float to2)
 		{
 			return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+		}
+
+		public static bool ApproximatelyZeroLength(this Vector3 vec)
+		{
+			if (vec == Vector3.zero) return true;
+			const float max = 0.001f;
+			var zx = Mathf.Abs(vec.x) <= max;
+			var zy = Mathf.Abs(vec.y) <= max;
+			var zz = Mathf.Abs(vec.z) <= max;
+			return zx && zy && zz;
 		}
 	}
 }
