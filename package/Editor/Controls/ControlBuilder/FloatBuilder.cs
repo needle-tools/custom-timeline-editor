f97bfbfc3172f044a71427db72b32152102bac88
Marcel Wiessler <marcel@gaisterhand.de>
2021-11-18 08:12:53 +0100

    add slider overflow rendering and power slider test

--- package/Editor/Controls/ControlBuilder/FloatBuilder.cs
+++ package/Editor/Controls/ControlBuilder/FloatBuilder.cs
@@ -18,34 +18,63 @@ namespace Needle.Timeline
 			view.label = "_";
 			view.RegisterValueChangedCallback(evt => { viewValue.SetValue(evt.newValue); });
 			view.RegisterValueChangedCallback(viewValue);
-			
+
 			var val = viewValue.GetValue();
-			if(val != null)
+			if (val != null)
 				view.value = (float)val;
 
 			var range = context?.Attributes?.GetCustomAttribute<RangeAttribute>();
-			if (range != null) 
+			if (range != null)
 			{
 				var slider = new Slider(range.min, range.max);
 				var value = viewValue.GetValue();
-				if(value != null)
+				if (value != null)
 					slider.value = (float)value;
 				slider.RegisterValueChangedCallback(evt =>
 				{
-					var val = evt.newValue;
-					view.SetValueWithoutNotify(val);
-					viewValue.SetValueWithoutNotify(val);
+					var _val = evt.newValue;
+					_val = CalculatePowerValue(_val, 2, range.min, range.max);
+					view.value = _val;
+					CheckSliderState();
 				});
 				view.RegisterValueChangedCallback(evt =>
 				{
-					view.value = Mathf.Clamp(view.value, range.min, range.max);
-					slider.SetValueWithoutNotify(view.value);
+					var _val = evt.newValue;
+					_val = CalculatePowerValueInverse(_val, 2f, range.min, range.max);
+					slider.SetValueWithoutNotify(_val);
+					CheckSliderState(); 
 				});
-				
+
+				void CheckSliderState()
+				{
+					if (view.value < range.min) 
+						slider.AddToClassList("overflow-min");
+					else if (view.value > range.max) 
+						slider.AddToClassList("overflow-max");
+					else
+					{
+						slider.RemoveFromClassList("overflow-min");
+						slider.RemoveFromClassList("overflow-max");
+					}
+				}
+
 				return Utils.MakeComposite(view, slider);
 			}
 
 			return view;
+		}
+
+		public static float CalculatePowerValue(float value, float power, float min, float max)
+		{
+			var val01 = value.Remap(min, max, 0, 1);
+			val01 = Mathf.Pow(val01, power);
+			value = val01.Remap(0, 1, min, max); 
+			return value;
+		}
+
+		public static float CalculatePowerValueInverse(float value, float power, float min, float max)
+		{
+			return CalculatePowerValue(value, 1 / power, min, max);
 		}
 	}
 }
