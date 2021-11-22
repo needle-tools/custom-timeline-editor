03ac380cdf4ce588fa646afcb642cdd05bac1300
Marcel Wiessler <marcel@gaisterhand.de>
2021-11-22 14:13:37 +0100

    start delete

---
+++ package/Editor/Commands/DeleteKeyframeData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Needle.Timeline.Commands
+{
+	public class DeleteTrackCommand : Command
+	{
+		private Object asset;
+		private JsonContainer jsonContainer;
+
+		public DeleteTrackCommand(Object asset, JsonContainer container)
+		{
+			this.asset = asset;
+			this.jsonContainer = container;
+		}
+		
+		protected override void OnRedo()
+		{
+			
+		}
+
+		protected override void OnUndo()
+		{
+			Debug.Log("UNDO DELETION");
+		}
+	}
+}
--- package/Editor/Serialization/AssetDeletionHandler.cs
+++ package/Editor/Serialization/AssetDeletionHandler.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using Needle.Timeline.Commands;
+using UnityEditor;
 using UnityEngine;
 
 namespace Needle.Timeline
@@ -11,6 +13,8 @@ namespace Needle.Timeline
 			CodeControlAsset.Deleted += OnDeleted;
 		}
 
+		private static List<Command> commands = new List<Command>();
+
 		private static void OnDeleted(CodeControlAsset obj)
 		{
 			// TODO: we could be nicer here and move assets elsewhere instead of just deleting using some custom undo action but for now that's it 
@@ -22,11 +26,23 @@ namespace Needle.Timeline
 					if (json.Id.StartsWith(obj.id))
 					{
 						Debug.Log("<b>DELETED</b> " + json.Id);
+						var deletion = new DeleteTrackCommand(obj, json){IsDone = true};
+						commands.Add(deletion);
+						EditorApplication.delayCall += RegisterCommands;
 						return true;
 					}
 				}
 				return false;
 			});
+		}
+
+		private static void RegisterCommands()
+		{
+			if (commands.Count <= 0) return;
+			var compound = commands.ToCompound("Deleted tracks");
+			commands.Clear();
+			Debug.Log("Register " + compound);
+			CustomUndo.Register(compound);
 		}
 	}
 }
