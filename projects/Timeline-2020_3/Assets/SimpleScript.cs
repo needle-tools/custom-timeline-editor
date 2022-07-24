17aa7c2b910bf419dbc9afc1587e6723acee4472
Marcel Wiessler <marcel@gaisterhand.de>
2022-07-24 16:12:58 +0200

    update 2020.3 project and add simple example

---
+++ projects/Timeline-2020_3/Assets/SimpleScript.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Needle.Timeline;
+using UnityEngine;
+
+public class SimpleScript : Animated
+{
+    public class Point
+    {
+        public Vector3 Position;
+        public float Radius = .1f;
+    }
+    
+    [Animate]
+    public List<Point> MyList;
+
+    private void OnDrawGizmos()
+    {
+        if (MyList == null) return;
+        foreach (var pt in MyList)
+        {
+            Gizmos.DrawSphere(pt.Position, pt.Radius);
+        }
+    }
+}
